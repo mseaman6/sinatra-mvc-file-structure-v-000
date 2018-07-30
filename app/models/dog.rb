c40828958d10b332a5ca2fab0609182bb9efaccb
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-30 16:03:32 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name, :breed, :age
   @@all = []
 
-  def initialize(:name = nil, breed: nil, age: nil)
+  def initialize(:name = nil, :breed = nil, :age = nil)
     @name = name
     @breed = breed
     @age = age
